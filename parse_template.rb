4ff29bcb429eb9d45fe65115cd4f2dbd74650158
rogerdpack <rogerpack2005@gmail.com>
2012-05-21 13:39:11 -0600

    working button size

--- parse_template.rb
+++ parse_template.rb
@@ -27,24 +27,22 @@ module ParseTemplate
   # returns a jframe that "matches" whatever the template says
   def self.parse_string string
     got = string
- #>>"[ a ] [ b ]".split(/(\[.*?\])/)
- # => ["", "[ a ]", " ", "[ b ]"]	
+    # >>"[ a ] [ b ]".split(/(\[.*?\])/)
+    # => ["", "[ a ]", " ", "[ b ]"]	
     frame = JFramer.new
 	current_y = 10
 	max_x = 100
     got.each_line{|l|
 	  current_x = 10
-	  if got =~ /\s*[-]+([\w ]+)[-]+\s*$/ # ----(a Title)---
-	    frame.set_title $1
-	  end
-	
 	  button_line_regex = /\[(.*?)\]/
 	  # >> "|  [Setup Preferences:preferences] [Start:start] [Stop:stop] |" .scan  /\[(.*?)\]/
 	  # => [["Setup Preferences:preferences"], ["Start:start"], ["Stop:stop"]]
-      if got =~ button_line_regex
-	    current_y += 25
+	  title_regex = /\s*[-]+([\w ]+)[-]+\s*$/  # ----(a Title)---
+	  if got =~ title_regex
+	    frame.set_title $1
+	  elsif got =~ button_line_regex
         got.scan(button_line_regex).each{|name|
-		  # name is like ["Setup Preferences:preferences"]
+		  # name is now like ["Setup Preferences:preferences"]
 		  name = name[0]
 		  if name.include? ':' # like "Start:start_button" ... disallows using colon at all, but hey...
 		    text = name.split(':')[0]
@@ -59,11 +57,11 @@ module ParseTemplate
           frame.panel.add button
           frame.buttons[name] = button		  
 		}
-	    
+	    current_y += 25
 	  end
 	  max_x = [max_x, current_x].max
 	}
-	frame.set_size max_x+15, current_y+15
+	frame.set_size max_x+25, current_y+45
 	frame
   end
 
