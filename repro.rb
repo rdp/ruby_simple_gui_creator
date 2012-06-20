042c5d8e3a71ae71b2a7fde8b0db1aa9f0117bca
rogerdpack <rogerpack2005@gmail.com>
2012-06-20 17:38:56 -0600

    still crash

--- repro.rb
+++ repro.rb
@@ -1,4 +1,12 @@
+require 'rubygems'
 require 'lib/simple_gui_creator'
-display_frame = ParseTemplate::JFramer.new.parse_setup_string "[bug:bug]"
-display_frame.elements[:bug].on_clicked { 	    SimpleGuiCreator.new_nonexisting_or_existing_filechooser_and_go "hello there" }
+#display_frame = ParseTemplate::JFramer.new.parse_setup_string "[bug:bug]"
 
+
+#display_frame.elements[:bug].on_clicked { 	SimpleGuiCreator.new_nonexisting_or_existing_filechooser_and_go "hello there" }
+begin
+SimpleGuiCreator.new_nonexisting_or_existing_filechooser_and_go "hello there"
+rescue
+puts 'here2'
+end
+puts 'here1'
