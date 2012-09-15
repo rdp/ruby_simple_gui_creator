f79c96238bfcf79c2abda5757d9cea8425ff2336
rogerdpack <rogerpack2005@gmail.com>
2012-09-15 16:11:42 -0600

    add enum method

--- lib/simple_gui_creator/ffmpeg_helpers.rb
+++ lib/simple_gui_creator/ffmpeg_helpers.rb
@@ -6,11 +6,11 @@ module FfmpegHelpers
   # use like vid_names = FfmpegHelpers.enumerate_directshow_devices[:video]
   # use like   name = DropDownSelector.new(nil, vid_names, "Select audio device to capture and stream").go_selected_value
   def self.enumerate_directshow_devices
-    ffmpeg_list_command = "ffmpeg.exe -list_devices true -f dshow -i dummy 2>&1"
+    ffmpeg_list_command = "ffmpeg -list_devices true -f dshow -i dummy 2>&1"
     enum = `#{ffmpeg_list_command}`
     unless enum.present?
-	  sleep 1
       p 'failed', enum
+	  sleep 1
 	  enum = `#{ffmpeg_list_command}`
       out = '2nd try resulted in :' + enum
       p out
@@ -26,8 +26,19 @@ module FfmpegHelpers
     {:audio => audio_names, :video => video_names}
   end
   
-  def self.warmup_ffmpeg_so_itll_be_disk_cached 
-    ffmpeg_list_command = "ffmpeg.exe -list_devices true -f dshow -i dummy 2>&1"
-    enum = `#{ffmpeg_list_command}`
+  def self.get_options_video_device name
+    ffmpeg_get_options_command = "ffmpeg  -list_options true -f dshow -i video=\"#{name}\" 2>&1"
+	enum = `#{ffmpeg_get_options_command}`
+	out = []
+	lines = enum.scan(/(pixel_format|vcodec)=([^ ]+)  min s=(\d+)x(\d+) fps=([^ ]+) max s=(\d+)x(\d+) fps=([^ ]+)$/)
+	lines.map{|video_type, video_type_name, min_x, min_y, min_fps, max_x, max_y, max_fps|
+	   {:video_type => video_type, :video_type_name => video_type_name, :min_x => min_x, 
+	    :max_x => max_x, :max_y => max_y, max_fps => max_fps}
+	}
   end
+  
+  def self.warmup_ffmpeg_so_itll_be_disk_cached 
+    system "ffmpeg -list_devices true -f dshow -i dummy 2>&1"
+  end
+  
 end
