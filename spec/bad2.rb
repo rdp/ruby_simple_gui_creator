35df9a1f2e4257ba63e40b8f738782b3ac297f21
rogerdpack <rogerpack2005@gmail.com>
2012-10-11 15:28:23 -0600

    still bad

--- spec/bad2.rb
+++ spec/bad2.rb
@@ -1,12 +1,9 @@
 # coding: UTF-8
 require '../lib/simple_gui_creator'
 include SimpleGuiCreator
-  template = <<-EOL
-  ------------ Recording Options -------------
-  "Save to file:" [✓:record_to_file]
-  EOL
+  template = %!"Save to file:" [✓:record_to_file]!
+  
   frame = ParseTemplate.new.parse_setup_string template
   frame.elements[:record_to_file].on_clicked { |new_value|
-    storage['record_to_file'] = new_value
-	reset_options_frame
+  
   }
