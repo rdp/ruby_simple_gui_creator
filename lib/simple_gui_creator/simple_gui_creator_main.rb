e220282ca1abf2e9bab199fddeeb59035f04318b
rogerdpack <rogerpack2005@gmail.com>
2012-12-19 13:59:28 -0700

    add method

--- lib/simple_gui_creator/simple_gui_creator_main.rb
+++ lib/simple_gui_creator/simple_gui_creator_main.rb
@@ -217,6 +217,16 @@ module SimpleGuiCreator
     end
   end
 
+  def self.run_later(seconds)
+    timer = javax.swing.Timer.new(seconds*1000, nil)
+	timer.repeats=false # don't loop, just fire once :)
+	timer.add_action_listener {
+	  yield
+	}
+	timer.start
+	puts 'started timer...'
+  end
+  
   def self.hard_exit!; java::lang::System.exit 0; end
   
   def self.invoke_in_gui_thread # sometimes I think I need this, but it never seems to help
