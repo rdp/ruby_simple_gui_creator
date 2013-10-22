7e53d552b01bedd1a6557dfd0ad7d4a8f73e0943
rogerdpack <rogerpack2005@gmail.com>
2013-10-22 14:56:48 -0600

    say what returns

--- lib/simple_gui_creator/simple_gui_creator_main.rb
+++ lib/simple_gui_creator/simple_gui_creator_main.rb
@@ -142,6 +142,7 @@ module SimpleGuiCreator
   end
     
   # prompts for user input, raises if they cancel the prompt or if they enter nothing
+  # returns nil if cancel or if blank and cancel_or_blank_ok=true
   def self.get_user_input(message, default = '', cancel_or_blank_ok = false)
     p 'please enter the information in the prompt:' + message[0..50] + '...'
     received = javax.swing.JOptionPane.showInputDialog(nil, message, default)
